5824c6e1e30445670329dab9f172f751e527c23a
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-05-15 16:50:36 +0400

    add STHTLapHistoryVC

---
+++ HippoTracker/Classes/STHTLapHistoryVC.h
@@ -0,0 +1,13 @@
+//
+//  STLapHistoryVC.h
+//  HippoTracker
+//
+//  Created by Maxim Grigoriev on 5/15/13.
+//  Copyright (c) 2013 Maxim Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STHTLapHistoryVC : UIViewController
+
+@end
---
+++ HippoTracker/Classes/STHTLapHistoryVC.m
@@ -0,0 +1,41 @@
+//
+//  STLapHistoryVC.m
+//  HippoTracker
+//
+//  Created by Maxim Grigoriev on 5/15/13.
+//  Copyright (c) 2013 Maxim Grigoriev. All rights reserved.
+//
+
+#import "STHTLapHistoryVC.h"
+
+@interface STHTLapHistoryVC ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+#pragma mark - view lifecycle
+
+@implementation STHTLapHistoryVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
